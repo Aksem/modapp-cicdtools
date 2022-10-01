929bee29f2c3ea144c72cfceaf5ad474235f6799
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-10-01 10:44:47 +0200

    Add debug logs

--- scripts/create_appimage.sh
+++ scripts/create_appimage.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 cp -R /app/scripts/squashfs-root/usr/** /usr/
+echo $1
+ls /app/$1_AppImage/usr/share/applications/
 appimagetool -s deploy /app/$1_AppImage/usr/share/applications/$1.desktop
 appimagetool /app/$1_AppImage/
