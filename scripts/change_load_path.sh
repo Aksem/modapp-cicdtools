58680ecc98fdce9518cb84b864019805235275b4
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-10-01 09:19:05 +0200

    Use python script for replacement instead sh

--- scripts/change_load_path.sh
+++ scripts/change_load_path.sh
@@ -9,24 +9,4 @@ poetry config virtualenvs.in-project true
 
 cd /app/
 
-NUITKA_PATH=$(poetry run python -c "import nuitka; import os; print(os.path.dirname(nuitka.__file__))")
-SOURCE_FILE="$NUITKA_PATH/build/static_src/MetaPathBasedLoader.c"
-
-STRING_TO_REPLACE="        appendStringSafe(filename, getBinaryDirectoryHostEncoded(), sizeof(filename));"
-NEW_STRING=$(cat <<-END
-        appendStringSafe(filename, getBinaryDirectoryHostEncoded(), sizeof(filename));
-        // new code
-        appendCharSafe(filename, SEP, sizeof(filename));
-        appendStringSafe(filename, "..", sizeof(filename));
-        appendCharSafe(filename, SEP, sizeof(filename));
-        appendStringSafe(filename, "usr", sizeof(filename));
-        appendCharSafe(filename, SEP, sizeof(filename));
-        appendStringSafe(filename, "lib", sizeof(filename));
-        // new code end
-END
-)
-
-# keep original copy
-cp $SOURCE_FILE /app/
-
-sed -i "s|$STRING_TO_REPLACE|$NEW_STRING|gi" $NUITKA_PATH/build/static_src/MetaPathBasedLoader.c
+poetry run python scripts/replace_load_path.py
---
+++ scripts/replace_load_path.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import nuitka
+
+
+STRING_TO_REPLACE = (
+    "        appendStringSafe(filename, getBinaryDirectoryHostEncoded(),"
+    " sizeof(filename));"
+)
+NEW_STRING = """        appendStringSafe(filename, getBinaryDirectoryHostEncoded(), sizeof(filename));
+        // new code
+        appendCharSafe(filename, SEP, sizeof(filename));
+        appendStringSafe(filename, "..", sizeof(filename));
+        appendCharSafe(filename, SEP, sizeof(filename));
+        appendStringSafe(filename, "usr", sizeof(filename));
+        appendCharSafe(filename, SEP, sizeof(filename));
+        appendStringSafe(filename, "lib", sizeof(filename));
+        // new code end
+"""
+
+
+def replace_load_path():
+    nuitka_path = Path(nuitka.__file__).parent
+    src_file_path = nuitka_path / "build" / "static_src" / "MetaPathBasedLoader.c"
+    with open(src_file_path, "r") as src_file:
+        src = src_file.read()
+
+    src.replace(STRING_TO_REPLACE, NEW_STRING)
+
+    with open(src_file_path, "w") as src_file:
+        src_file.write(src)
+
+
+if __name__ == "__main__":
+    replace_load_path()
