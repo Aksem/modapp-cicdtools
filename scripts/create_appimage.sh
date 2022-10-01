2562f527cdfd7fcf9e99bcc96ac313d518e53cf9
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-10-01 12:32:35 +0200

    Add AppImage version

--- scripts/create_appimage.sh
+++ scripts/create_appimage.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# $1 .. app name
+# $2 .. app version
+
 cp -R /app/scripts/squashfs-root/usr/** /usr/
-echo $1
-ls /app/$1_AppImage/usr/share/applications/
+
+export VERSION=$2
+
 appimagetool -s deploy /app/$1_AppImage/usr/share/applications/$1.desktop
 appimagetool /app/$1_AppImage/
