9c6d15fb1b35caf644547552854f238893606853
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-10-01 08:19:05 +0200

    Add build deps for package compilation

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-apk add bash
+PYTHON_BUILD_DEPS="build-base libffi-dev openssl-dev bzip2-dev zlib-dev readline-dev sqlite-dev xz-dev"
+
+apk add bash $PYTHON_BUILD_DEPS
 
 export PYENV_ROOT="$HOME/.pyenv"
 eval "$(pyenv init -)"
