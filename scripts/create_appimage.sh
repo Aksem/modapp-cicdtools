0d0f79930d2204e6827928539819a1b95ce308be
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-10-01 07:58:41 +0200

    AppImage build in CI

--- scripts/create_appimage.sh
+++ scripts/create_appimage.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 cp -R /app/scripts/squashfs-root/usr/** /usr/
-appimagetool -s deploy app/usr/share/applications/app.desktop
-appimagetool app
-
+appimagetool -s deploy /app/$1_AppImage/usr/share/applications/$1.desktop
+appimagetool /app/$1_AppImage/
