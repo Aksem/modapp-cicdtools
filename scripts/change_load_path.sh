d46855b25e07a181d75d14223b6d63fe218d0f05
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-10-02 10:09:33 +0200

    Try to build without pyenv setup each time

--- scripts/change_load_path.sh
+++ scripts/change_load_path.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# apk add bash
-
-export PYENV_ROOT="$HOME/.pyenv"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# export PYENV_ROOT="$HOME/.pyenv"
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
 poetry config virtualenvs.in-project true
 
 cd /app/
--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# PYTHON_BUILD_DEPS="openssl-dev bzip2-dev zlib-dev readline-dev sqlite-dev xz-dev"
-
-# apk add $PYTHON_BUILD_DEPS
-
 export PYENV_ROOT="$HOME/.pyenv"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
